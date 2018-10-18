9000d8ea2060da2ce2bdc6c9b611aba3a572cf7a
cpasjuste <cpasjuste@gmail.com>
2018-10-18 10:06:30 +0200

    config: done

--- examples/config/source/main.cpp
+++ examples/config/source/main.cpp
@@ -7,6 +7,15 @@
 using namespace c2d;
 using namespace c2d::config;
 
+void addConfigSection(Config *config) {
+
+    Section section("DEMO");
+    section.add({"INTEGER", 10});
+    section.add({"STRING", "Hello World"});
+    section.add({"FLOAT", 9.99f});
+    config->add(section);
+}
+
 int main() {
 
     // create the main renderer
@@ -16,22 +25,37 @@ int main() {
     // create io helper
     auto *io = new C2DIo();
 
-    // TODO
+    // create a configuration named "C2D_CFG" in a writable directory (getHomePath)
     Config *config = new Config("C2D_CFG", io->getHomePath() + "config.cfg");
-
-    Section section("DEMO");
-    section.add({"INTEGER", 10});
-    section.add({"STRING", "Hello World"});
-    section.add({"FLOAT", 9.99f});
-    config->add(section);
-
-    if(!config->load()) {
-        // file doesn't exist or is not well formed, (re)create it
+    // add a section to the configuration
+    addConfigSection(config);
+    // load the configuration from file, overwriting default values (added in addConfigSection)
+    if (!config->load()) {
+        // file doesn't exist or is malformed, (re)create it
         config->save();
     }
 
-    printf("%i\n", section.getOptions().at(0).getInteger());
-    printf("%i\n", config->getSection("DEMO").getOption("INTEGER").getInteger());
+    // here the "INTEGER" option in section "DEMO" will be overwritten by the "config.cfg" file parameter, if any
+    printf("DEMO->INTEGER = %i\n", config->getOption("DEMO", "INTEGER")->getInteger());
+
+    // loop through all sections and options
+    for (auto &section : *config->getSections()) {
+        printf("\n%s\n", section.getName().c_str());
+        for (auto &option : *section.getOptions()) {
+            if (option.getType() == Option::Type::Integer) {
+                printf("\t%s: %i\n", option.getName().c_str(), option.getInteger());
+            } else if (option.getType() == Option::Type::Float) {
+                printf("\t%s: %f\n", option.getName().c_str(), option.getFloat());
+            } else {
+                printf("\t%s: %s\n", option.getName().c_str(), option.getString().c_str());
+            }
+        }
+    }
+
+    // change an option and save the new configuration, next time we load this example
+    // the "INTEGER" from section "DEMO" option will return "20"
+    config->getOption("DEMO", "INTEGER")->setInteger(20);
+    config->save();
 
     // main loop
     while (renderer->getElapsedTime().asSeconds() < 10) {
