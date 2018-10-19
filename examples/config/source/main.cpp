e14bb67059a5e69476f8aba98fffe697db9d026c
david <cpasjuste@gmail.com>
2018-10-20 01:03:40 +0200

    config: add recursion support

--- examples/config/source/main.cpp
+++ examples/config/source/main.cpp
@@ -7,13 +7,21 @@
 using namespace c2d;
 using namespace c2d::config;
 
-void addConfigSection(Config *config) {
+void addConfigGroup(Config *config) {
 
-    Section section("DEMO");
-    section.addOption({"INTEGER", 10});
-    section.addOption({"STRING", "Hello World"});
-    section.addOption({"FLOAT", 9.99f});
-    config->addSection(section);
+    // add a main config group
+    Group group("DEMO");
+    group.addOption({"INTEGER", 10});
+    group.addOption({"STRING", "Hello World"});
+    group.addOption({"FLOAT", 9.99f});
+
+    // add a child group
+    Group child("CHILD");
+    child.addOption({"INTEGER", 777});
+    group.addGroup(child);
+
+    // add all that to the main config
+    config->addGroup(group);
 }
 
 int main() {
@@ -24,21 +32,23 @@ int main() {
 
     // create a configuration named "C2D_CFG" in a writable directory (getHomePath)
     Config *config = new Config("C2D_CFG", renderer->getIo()->getHomePath() + "config.cfg");
-    // add a section to the configuration
-    addConfigSection(config);
-    // load the configuration from file, overwriting default values (added in addConfigSection)
+    // add a group to the configuration
+    addConfigGroup(config);
+    // load the configuration from file, overwriting default values (added in addConfigGroup)
     if (!config->load()) {
         // file doesn't exist or is malformed, (re)create it
         config->save();
     }
 
-    // here the "INTEGER" option in section "DEMO" will be overwritten by the "config.cfg" file parameter, if any
+    // here the "INTEGER" option in group "DEMO" will be overwritten by the "config.cfg" file parameter, if any
+    // note that you would normally check for a null pointer before "getInteger"
     printf("DEMO->INTEGER = %i\n", config->getOption("DEMO", "INTEGER")->getInteger());
 
-    // loop through all sections and options
-    for (auto &section : *config->getSections()) {
-        printf("\n%s:\n", section.getName().c_str());
-        for (auto &option : *section.getOptions()) {
+
+    // loop through all groups and options
+    for (auto &group : *config->getGroups()) {
+        printf("\n%s:\n", group.getName().c_str());
+        for (auto &option : *group.getOptions()) {
             if (option.getType() == Option::Type::Integer) {
                 printf("\t%s: %i\n", option.getName().c_str(), option.getInteger());
             } else if (option.getType() == Option::Type::Float) {
@@ -50,7 +60,8 @@ int main() {
     }
 
     // change an option and save the new configuration, next time we load this example
-    // the "INTEGER" from section "DEMO" option will return "20"
+    // the "INTEGER" option from "DEMO" group will return "20"
+    // note that you would normally check for a null pointer before "getInteger"
     config->getOption("DEMO", "INTEGER")->setInteger(20);
     config->save();
 
